a918d0e588e5c5d29d9104dae7016ecdce15d16f
gaurav-singh-panwar <55618776+gaurav-singh-panwar@users.noreply.github.com>
2023-08-23 16:17:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,62 +1,64 @@
-//your JS code here. If required.
-const images = [
-  "url-to-image-1",
-  "url-to-image-2",
-  "url-to-image-3",
-  "url-to-image-4",
-  "url-to-image-5"
-];
+//your code here
+const main = document.querySelector("main");
+const verify = document.getElementById("verify");
+const reset = document.getElementById("reset");
+const para = document.getElementById("para");
 
-// Choose a random image to repeat
-const repeatImage = images[Math.floor(Math.random() * images.length)];
+// console.log(main);
+let arrClasses = ["img1" ,"img2","img3","img4","img5" ]
+let randomPhotoClass = arrClasses[Math.round(Math.random()*5)];
+// console.log(randomPhotoUrl);
+const randomPhoto = document.createElement("img");
+randomPhoto.className = randomPhotoClass;
+main.appendChild(randomPhoto);
 
-// Add a duplicate image to the array
-images.push(repeatImage);
 
-// Shuffle the array
-for (let i = images.length - 1; i > 0; i--) {
-  const j = Math.floor(Math.random() * (i + 1));
-  [images[i], images[j]] = [images[j], images[i]];
-}
-
-// Assign class names to each image tag
-const imgTags = document.querySelectorAll("img");
-for (let i = 0; i < imgTags.length; i++) {
-  imgTags[i].classList.add("img" + (i+1));
-  imgTags[i].src = images[i];
-}
-
-let selectedImages = [];
-
-// Add click event listeners to images
-imgTags.forEach(img => {
-  img.addEventListener("click", () => {
-    if (selectedImages.length < 2) {
-      img.classList.add("selected");
-      selectedImages.push(img);
-    }
-    if (selectedImages.length === 2) {
-      const verifyButton = document.getElementById("verify");
-      verifyButton.classList.remove("hidden");
-      verifyButton.addEventListener("click", () => {
-        if (selectedImages[0].classList[0] === selectedImages[1].classList[0]) {
-          document.getElementById("para").textContent = "You are a human. Congratulations!";
-        } else {
-          document.getElementById("para").textContent = "We can't verify you as a human. You selected the non-identical tiles.";
+const imgs = document.getElementsByTagName("img");
+let clicked = 0;
+let selected = [];
+for (let i=0 ;i<imgs.length;i++){
+    const currentImg = imgs[i];
+    currentImg.addEventListener("click",()=>{
+        selected.push(currentImg);
+        clicked++;
+        currentImg.classList.add("selected");  
+        if(clicked === 1){
+              reset.style.display = "flex";
+            reset.addEventListener("click",()=>{
+                clicked = 0;
+                for(let val of selected) val.classList.remove("selected");
+                reset.style.display = "none"; 
+                para.style.display = "none";
+            })
         }
-        verifyButton.classList.add("hidden");
-      });
+        else if(clicked === 2){
+            verify.style.display = "flex";
+        }
+        else{
+            verify.style.display = "none";
+        }
+    })
+}
+
+verify.addEventListener("click",()=>{
+    let classes = [];
+    for(let val of selected){
+        classes.push(val.className);
     }
-    const resetButton = document.getElementById("reset");
-    resetButton.classList.remove("hidden");
-    resetButton.addEventListener("click", () => {
-      selectedImages.forEach(img => {
-        img.classList.remove("selected");
-      });
-      selectedImages = [];
-      document.getElementById("verify").classList.add("hidden");
-      resetButton.classList.add("hidden");
-      document.getElementById("para").textContent = "";
-    });
-  });
-});
+    let sameClass = true;
+    for(let i = 0; i < classes.length-1; i++){
+         if(classes[i] !== classes[i+1]){
+            sameClass = false;
+            break;
+         }
+    }
+    if(sameClass === true){
+        para.style.display = "flex";
+       para.innerText = "You are a human. Congratulations!";
+    }
+    else{
+        para.style.display = "flex";
+        para.innerText = "We can't verify you as a human. You selected the non-identical tiles.";
+    }
+    verify.style.display = "none";
+})
